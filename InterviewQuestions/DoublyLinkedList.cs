683d2e8fcb424f90a3c09fcf3bf4d58316f2ef24
Bryan Lipscy <bryan@lipscy.net>
2020-03-11 20:13:56 -0700

    Add project files.

---
+++ InterviewQuestions/DoublyLinkedList.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewQuestions
+{
+    // Interview question: Perform a deep copy of a doubly linked list where the next or previous pointers
+    // may point to other nodes in the list. 
+
+    public class DoubleLinkNode
+    {
+        public DoubleLinkNode PreviousNode { get; set; }
+        public DoubleLinkNode NextNode { get; set; }
+        public string Title { get; set; }
+
+        public DoubleLinkNode(string title) : this()
+        {
+            Title = title;
+        }
+
+        public DoubleLinkNode()
+        {
+            this.PreviousNode = null;
+            this.NextNode = null;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+
+        public DoubleLinkNode CopyNode()
+        {
+            return new DoubleLinkNode()
+            {
+                Title = this.Title
+            };
+        }
+    }
+
+    public class DoubleLinkedList
+    {
+        private DoubleLinkNode m_Frist;
+        
+        public bool IsEmpty
+        {
+            get
+            {
+                return m_Frist == null;
+            }
+        }
+
+        public DoubleLinkNode Insert(string title)
+        {
+            return this.Insert(new DoubleLinkNode(title));
+        }
+
+        public DoubleLinkNode Insert(DoubleLinkNode newNode)
+        {
+            return this.InsertLast(newNode);
+        }
+
+        public void InsertFirst(DoubleLinkNode newNode)
+        {
+            if (this.m_Frist == null)
+            {
+                // This is the first node now
+                this.m_Frist = newNode;
+            }
+            else
+            {
+                newNode.NextNode = this.m_Frist;
+                this.m_Frist.PreviousNode = newNode;
+            }
+        }
+
+        public void InsertAfter(DoubleLinkNode node, DoubleLinkNode newNode)
+        {
+            newNode.NextNode = node.NextNode;
+            node.NextNode = newNode;
+            newNode.PreviousNode = node;
+            newNode.NextNode.PreviousNode = newNode;
+        }
+
+         public void InsertBefore(DoubleLinkNode node, DoubleLinkNode newNode)
+        {
+            var previousNode = node.PreviousNode;
+
+            newNode.PreviousNode = previousNode;
+            newNode.NextNode = node;
+
+            previousNode.NextNode = newNode;
+            node.PreviousNode = newNode;
+        }
+
+        public DoubleLinkNode InsertLast(DoubleLinkNode newNode)
+        {
+            // Starting at the head to find the last node in the list
+            DoubleLinkNode currentNode = this.m_Frist;
+            var previousNode = this.m_Frist;
+            while(currentNode != null)
+            {
+                previousNode = currentNode;
+                currentNode = currentNode.NextNode;
+            }
+
+            if (previousNode == null)
+            {
+                this.InsertFirst(newNode);
+            }
+            else
+            {
+                // Current node is now pointing to the last node in the list
+                previousNode.NextNode = newNode;
+                newNode.PreviousNode = previousNode;
+                newNode.NextNode = null;
+            }
+
+            return newNode;
+        }
+
+        public DoubleLinkNode GetLastNode()
+        {
+            DoubleLinkNode currentNode = this.m_Frist;
+            DoubleLinkNode previousNode = this.m_Frist;
+
+            while(currentNode != null)
+            {
+                previousNode = currentNode;
+                currentNode = currentNode.NextNode;
+            }
+
+            return previousNode;
+        }
+
+        public DoubleLinkedList DeepCopy()
+        {
+            var newList = new DoubleLinkedList();
+            DoubleLinkNode currentNode = this.m_Frist;
+
+            var newNode = currentNode.CopyNode();
+            newList.Insert(newNode);
+            
+            currentNode = currentNode.NextNode;
+
+            while(currentNode != null)
+            {
+                newNode = currentNode.CopyNode();
+                newList.Insert(newNode);
+                currentNode = currentNode.NextNode;
+            }
+
+            currentNode = this.GetLastNode();
+
+            return newList;
+        }
+    }
+}
---
+++ InterviewQuestions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace InterviewQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = CopyDoublyLinkedList();
+        }
+
+        static DoubleLinkedList CopyDoublyLinkedList()
+        {
+            var dlList = new DoubleLinkedList();
+            dlList.Insert("First");
+            dlList.Insert("Second");
+            dlList.Insert("Third");
+            dlList.Insert("Fourth");
+            dlList.Insert("Fifth");
+            dlList.Insert("Sixth");
+            dlList.Insert("Seventh");
+            dlList.Insert("Eighth");
+            dlList.Insert("Nineth");
+            dlList.Insert("Tenth");
+
+            return dlList.DeepCopy();
+        }
+
+        // TODO: Finish this.
+        static void CopyDoublyLinkedListWithLoopReference()
+        {
+            var dlList = new DoubleLinkedList();
+            var item1 = dlList.Insert("First");
+            var item2 = dlList.Insert("Second");
+            var item3 = dlList.Insert("Third");
+            var item4 = dlList.Insert("Fourth");
+            var item5 = dlList.Insert("Fifth");
+
+            item1.NextNode = item4;
+            item4.PreviousNode = item2;
+
+            var newcopy = dlList.DeepCopy();
+        }
+
+    }
+}
